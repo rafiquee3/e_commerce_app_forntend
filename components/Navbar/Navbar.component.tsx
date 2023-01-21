159374915d9fffbc91c6ec1c0fd2c6ec49dbd6e9
Rafał Sokołowski <rafiquee.code@gmail.com>
2023-01-21 22:57:02 +0100

    Feat: item icon, counter

--- components/Navbar/Navbar.component.tsx
+++ components/Navbar/Navbar.component.tsx
@@ -1,28 +1,73 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { FC } from 'react';
+import styled from "styled-components";
 import { LoginBttn } from '.';
 import { Menu } from ".";
+import { useCartStore } from '../Store/store';
 
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  padding-right: 2rem;
+  width: 100%;
+  height: 40px;
+  background: white;
+  font-size: 18px;
+  color: black;
+  z-index: 999;
+
+  .logo {
+    margin-left: calc(2rem + 40px);
+
+    div {
+      position: absolute;
+      line-height: 40px;
+      color: black;
+    }
+  }
+  .actionPanel {
+    position: relative;
+    display: flex;
+    align-items: center;
+
+    .itemCounter {
+      position: absolute;
+      left: -70px;
+      line-height: 40px;
+    }
+    .cart {
+      position: absolute;
+      left: -40px;
+      top: 5px;
+      line-height: 40px;
+    }
+  }
+`
 export const Navbar: FC = (): JSX.Element => {
+  const { cartItems } = useCartStore();
   return (
-    <nav 
-      style={{
-        position: 'fixed',
-        top: '0px',
-        display: 'flex',
-        justifyContent: 'space-between',
-       
-        paddingRight: '2rem',
-        width: '100%',
-        height: '40px',
-        background: '#F0F4F4',
-        fontSize: 18,
-        color: 'white',
-        zIndex: '999',
-      }}
-    >
-      <div style={{marginLeft: 'calc(2rem + 40px)'}}><div style={{position: 'absolute', lineHeight: '40px', color: 'black'}}>SKLEP</div></div>
+    <Nav>
+      <div className='logo'>
+        <div>
+          SKLEP
+        </div>
+      </div>
       <Menu/>
-      <LoginBttn/>
-    </nav>
+      <div className='actionPanel'>
+        <div className='itemCounter'>{cartItems.length && cartItems.reduce((acc, curr) => acc + curr.quantity, 0)}</div>
+        <Link className='cart' href={'/'}>
+          <Image
+            src="/cart.png"
+            alt="Cart icon"
+            width={30}
+            height={30}
+          />
+        </Link>
+        <LoginBttn/>
+      </div>
+    </Nav>
   )
 }
