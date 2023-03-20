6c2bd3ac296b99765387018130767981060f9c91
Rafał Sokołowski <rafiquee.code@gmail.com>
2023-03-20 21:16:04 +0100

    Feat: img url bug fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["i.ibb.co"],
+  },
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
